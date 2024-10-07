4f834b14a95ef36c60a323ea83625a031d1514d7
aymanebs <aymane.bisdaoune@gmail.com>
2024-10-07 10:16:44 +0100

    minor fix in password validation regex

--- src/api/v1/middlewares/validateRegister.js
+++ src/api/v1/middlewares/validateRegister.js
@@ -4,7 +4,7 @@ const registerSchema = joi.object({
 
     name:joi.string().min(3).max(30).required(),
     email:joi.string().email().required(),
-    password:joi.string().pattern(new RegExp("/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/")).required(),
+    password:joi.string().pattern(new RegExp("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?!.* ).{8,16}$")).required(),
     confirm_password:joi.string().valid(joi.ref('password')).required(),
     address :joi.string().min(3).max(50).required(),
     phone:joi.string().length(10).required(),
